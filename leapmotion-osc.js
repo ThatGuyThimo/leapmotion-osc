72336972d82990d02b3d8c395300384d95a37ef7
voro282 <43708321+Zedzeen@users.noreply.github.com>
2022-02-20 20:41:30 +0100

    Some standard npm stuff

--- leapmotion-osc.js
+++ leapmotion-osc.js
@@ -1,35 +1,26 @@
-/* const Server = require('./node_modules/node-osc/dist/lib/Server'); */
 const Client = require('./node_modules/node-osc/dist/lib/Client');
-var Leap = require('leapjs');
-
-/* var oscServer = new Server(9001, '0.0.0.0', () => {
-	console.log('OSC Server is listening');
-}); */
+const Leap = require('leapjs');
 
 const client = new Client('127.0.0.1', 9000);
 
-/* oscServer.on('message', function (msg) {
-	console.log('Message: ' + msg);
-}); */
-
-Leap.loop({optimizeHMD:true}, function(frame) {
+Leap.loop({optimizeHMD:true}, (frame) => {
 	frame.hands.forEach(hand => {
 		hand.fingers.forEach(finger => {
-			var osc_path = '/avatar/parameters/' + hand.type + fingerType(finger.type);
+			const osc_path = '/avatar/parameters/' + hand.type + fingerType(finger.type);
 
-			var finger_x = finger.dipPosition[0];
-			var finger_y = finger.dipPosition[1];
-			var finger_z = finger.dipPosition[2];
+			const finger_x = finger.dipPosition[0];
+			const finger_y = finger.dipPosition[1];
+			const finger_z = finger.dipPosition[2];
 
-			var hand_x = hand.palmPosition[0];
-			var hand_y = hand.palmPosition[1];
-			var hand_z = hand.palmPosition[2];
+			const hand_x = hand.palmPosition[0];
+			const hand_y = hand.palmPosition[1];
+			const hand_z = hand.palmPosition[2];
 
-			var dx = finger_x - hand_x;
-			var dy = finger_y - hand_y;
-			var dz = finger_z - hand_z;
+			const dx = finger_x - hand_x;
+			const dy = finger_y - hand_y;
+			const dz = finger_z - hand_z;
 			
-			var dist = (Math.sqrt(Math.pow(dx, 2) + Math.pow(dy, 2) + Math.pow(dz, 2)));
+			const dist = (Math.sqrt(Math.pow(dx, 2) + Math.pow(dy, 2) + Math.pow(dz, 2)));
 
 			client.send(osc_path, (dist / 100));
 			console.log('Send: ' + osc_path + ',' + (dist / 100));

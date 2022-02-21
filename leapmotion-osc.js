a7ae70b9521ca8db7edcacb2237721d88d8a6737
adeleine1412 <53810001+adeleine1412@users.noreply.github.com>
2022-02-21 20:06:12 +0100

    pull changes from Zedzeen/leapmotion-osc

--- leapmotion-osc.js
+++ leapmotion-osc.js
@@ -7,26 +7,31 @@ Leap.loop({optimizeHMD:true}, (frame) => {
 	frame.hands.forEach(hand => {
 		hand.fingers.forEach(finger => {
 			const osc_path = '/avatar/parameters/' + hand.type + fingerType(finger.type);
+			const osc_value = getDistanceBetween(finger.dipPosition, hand.palmPosition);
 
-			const finger_x = finger.dipPosition[0];
-			const finger_y = finger.dipPosition[1];
-			const finger_z = finger.dipPosition[2];
-
-			const hand_x = hand.palmPosition[0];
-			const hand_y = hand.palmPosition[1];
-			const hand_z = hand.palmPosition[2];
-
-			const dx = finger_x - hand_x;
-			const dy = finger_y - hand_y;
-			const dz = finger_z - hand_z;
-			
-			const dist = (Math.sqrt(Math.pow(dx, 2) + Math.pow(dy, 2) + Math.pow(dz, 2)));
-
-			client.send(osc_path, (dist / 100));
-			console.log('Send: ' + osc_path + ',' + (dist / 100));
+			client.send(osc_path, osc_value);
+			console.log('Send: ' + osc_path + ',' + osc_value);
 		})
 	});
 });
+
+function getDistanceBetween(pointA, pointB) {
+	const ax = pointA[0];
+	const ay = pointA[1];
+	const az = pointA[2];
+
+	const bx = pointB[0];
+	const by = pointB[1];
+	const bz = pointB[2];
+
+	const cx = ax - bx;
+	const cy = ay - by;
+	const cz = az - bz;
+
+	const dist = (Math.sqrt(Math.pow(cx, 2) + Math.pow(cy, 2) + Math.pow(cz, 2)));
+	
+	return dist / 100;
+}
 
 function fingerType(type) {
 	switch(type) {

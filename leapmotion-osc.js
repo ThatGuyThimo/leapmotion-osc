d619086cff4922e82f803dbb05ec0316e3f051a5
adeleine1412 <53810001+adeleine1412@users.noreply.github.com>
2022-02-21 20:35:18 +0100

    turn distance calculation into a function

--- leapmotion-osc.js
+++ leapmotion-osc.js
@@ -1,19 +1,49 @@
 const Client = require('./node_modules/node-osc/dist/lib/Client');
-const Leap = require('leapjs');
+const Server = require('./node_modules/node-osc/dist/lib/Server');
 
+const Leap = require('leapjs');
 const client = new Client('127.0.0.1', 9000);
+const oscServer = new Server(9001, '0.0.0.0', () => {
+	console.log('OSC Server is listening');
+});
+
+oscServer.on('message', function (msg) {
+	console.log('Message: ' + msg);
+});
 
 Leap.loop({optimizeHMD:true}, (frame) => {
 	frame.hands.forEach(hand => {
+
+		if (hand.type == 0) {
+
+			const imTrackingThumbLeft = getDistanceBetween(hand.fingers[0].dipPosition, hand.palmPosition);
+			const imTrackingIndexLeft = getDistanceBetween(hand.fingers[1].dipPosition, hand.palmPosition);
+			const imTrackingMiddleLeft = getDistanceBetween(hand.fingers[2].dipPosition, hand.palmPosition);
+			const imTrackingRingLeft = getDistanceBetween(hand.fingers[3].dipPosition, hand.palmPosition);
+			const imTrackingPinkyLeft = getDistanceBetween(hand.fingers[4].dipPosition, hand.palmPosition);
+
+		} else {
+
+			const imTrackingThumbRight = getDistanceBetween(hand.fingers[0].dipPosition, hand.palmPosition);
+			const imTrackingIndexRight = getDistanceBetween(hand.fingers[1].dipPosition, hand.palmPosition);
+			const imTrackingMiddleRight = getDistanceBetween(hand.fingers[2].dipPosition, hand.palmPosition);
+			const imTrackingRingRight = getDistanceBetween(hand.fingers[3].dipPosition, hand.palmPosition);
+			const imTrackingPinkyRight = getDistanceBetween(hand.fingers[4].dipPosition, hand.palmPosition);
+			
+		}
+
 		hand.fingers.forEach(finger => {
 			const osc_path = '/avatar/parameters/' + hand.type + fingerType(finger.type);
 			const osc_value = getDistanceBetween(finger.dipPosition, hand.palmPosition);
-
-			client.send(osc_path, osc_value);
-			console.log('Send: ' + osc_path + ',' + osc_value);
 		})
+
 	});
 });
+
+function sendOSC(path, value) {
+	client.send(path, value);
+	console.log('Send: ' + path + ',' + value);
+}
 
 function getDistanceBetween(pointA, pointB) {
 	const ax = pointA[0];
@@ -28,9 +58,9 @@ function getDistanceBetween(pointA, pointB) {
 	const cy = ay - by;
 	const cz = az - bz;
 
-	const dist = (Math.sqrt(Math.pow(cx, 2) + Math.pow(cy, 2) + Math.pow(cz, 2)));
+	const dist = ((Math.sqrt(Math.pow(cx, 2) + Math.pow(cy, 2) + Math.pow(cz, 2))) / 100);
 	
-	return dist / 100;
+	return dist;
 }
 
 function fingerType(type) {
